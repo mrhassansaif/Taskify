3ed066f0a12cf6a8034de94ae4e249af6ff76e4a
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2023-03-24 15:59:54 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Taskify the TaskMaster: A robust and user-friendly TODO app built with React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
