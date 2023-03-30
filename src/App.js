0161dc052657cf12845f5ef3bd69206aefaa491a
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2023-03-30 20:23:53 +0500

    Added Todo UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './Components/Auth/SignUp';
+import Todo from './Components/Todo/Todo';
 
 function App() {
   return (
     <>
-    <SignUp/>
+    {/* <SignUp/> */}
+    <Todo/>
     
     </>
   );
