d1d4a7612e9cdcd86e1fa4d6e7955a51b3bae774
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2023-03-30 22:53:07 +0500

    Testting

---
+++ src/Components/Auth/test.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function test() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim()) {
+      setTodos([
+        ...todos,
+        { id: Date.now(), text: inputValue, completed: false },
+      ]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleCompleteTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleClearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+
+  const [filter, setFilter] = useState("all");
+
+  const handleFilterChange = (filter) => {
+    setFilter(filter);
+  };
+
+  const filteredTodos =
+    filter === "all"
+      ? todos
+      : filter === "active"
+      ? todos.filter((todo) => !todo.completed)
+      : todos.filter((todo) => todo.completed);
+
+  return <div>test</div>;
+}
--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Todo.css"
 
+
+
+
 export default function Todo() {
   return (
     <>
