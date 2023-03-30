55db13d956df6ac7210ca8ca4b7107648b8626d2
Muhammad Hassan Bin Saif <hassansaif0ki@gmail.com>
2023-03-30 22:57:36 +0500

    Added

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -17,7 +17,7 @@ export default function Todo() {
 
             <p class="h1 text-center mt-3 mb-4 pb-3 text-primary">
               <i class="fas fa-check-square me-1"></i>
-              <u>My Todo-s</u>
+              <u> Taskify</u>
             </p>
 
             <div class="pb-2">
